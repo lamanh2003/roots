fe745d43082dc791586de310f3932912d1b9f889
Nguyễn Hoàng Phương <phuongnh.gplaystudio@gmail.com>
2023-02-04 13:46:11 +0700

    update node

---
+++ Assets/Script/Utilities/EditorNodeUtility.cs
@@ -0,0 +1,22 @@
+using Base;
+using Controller;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class EditorNodeUtility : MonoBehaviour
+{
+    public Node.LineColor chosenColor;
+    public Node node;
+
+    [Button]
+    public void AddLeftNode()
+    {
+        GamePlayController.Singleton.nodeController.AddNode(this.node, isLeftPriority: true, chosenColor);
+    }
+    
+    [Button]
+    public void AddRightNode()
+    {
+        GamePlayController.Singleton.nodeController.AddNode(this.node, isLeftPriority: true, chosenColor);
+    }
+}
