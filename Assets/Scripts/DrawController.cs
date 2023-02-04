c4a14127d975cceda12c0dc260e4e367057d9422
tiennlv <lvt11072000@gmail.com>
2023-02-04 10:09:03 +0700

    tien add line

---
+++ Assets/Scripts/DrawController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawController : MonoBehaviour
+{
+    public DrawPoint drawPoint;
+
+    private Vector3 mousePosition;
+    private bool isDrawing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        drawPoint.SetPosition(new Vector3(mousePosition.x, mousePosition.y, 0f));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDrawing = true;
+            drawPoint.StartDraw();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDrawing = false;
+            drawPoint.StopDraw();
+        }
+    }
+}
