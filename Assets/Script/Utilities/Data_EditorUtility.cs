02c922dcecf8a2be8678999a14632335337eb379
Nguyễn Hoàng Phương <phuongnh.gplaystudio@gmail.com>
2023-02-04 16:10:30 +0700

    lv1

--- Assets/Script/Utilities/Data_EditorUtility.cs
+++ Assets/Script/Utilities/Data_EditorUtility.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Base;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Data_EditorUtility : MonoBehaviour
 {
+    public Transform rootNodeTransform;
+    
     public int level;
+    
     [Button]
     public void CreateLevelData()
     {
-        
+        List<Data_Node> firstLevelNodes = new List<Data_Node>();
+        foreach (Transform childOfRoot in rootNodeTransform)
+        {
+            Data_Node dataNode_1 = new Data_Node();
+            dataNode_1.dataNodeType = Data_Node_Type.NormalColor;
+            dataNode_1.lineColor = childOfRoot.GetComponent<Node>().lineColor;
+            dataNode_1.childNodes = new List<Data_Node>();
+            Transform transformToCheck = childOfRoot;
+            while (true)
+            {
+                if (transformToCheck.childCount == 0)
+                {
+                    break;
+                }
+
+                foreach (Transform childOfTransformToCheck in transformToCheck)
+                {
+                }
+            }
+        }
     }
 }
--- Assets/Script/Utilities/Data_Node.cs
+++ Assets/Script/Utilities/Data_Node.cs
@@ -1,8 +1,16 @@
 using System;
 using System.Collections.Generic;
+using Base;
 
 [Serializable]
 public class Data_Node
 {
+    public Data_Node_Type dataNodeType;
+    public Node.LineColor lineColor;
     public List<Data_Node> childNodes;
+}
+
+public enum Data_Node_Type
+{
+    NormalColor
 }
